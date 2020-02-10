d9462b3deb3f749935c89287221671ec7110154f
Julio Pineda <Julio Pineda@LAPTOP-SIFK8H4K>
2020-02-10 16:48:08 -0600

    terminado

--- julio_cajero_primo_fibonacci/Form1.Designer.cs
+++ julio_cajero_primo_fibonacci/Form1.Designer.cs
@@ -28,12 +28,294 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnRestirar = new System.Windows.Forms.Button();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.lblRespuesta = new System.Windows.Forms.Label();
+            this.lblCantidad = new System.Windows.Forms.Label();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.btnmio = new System.Windows.Forms.Button();
+            this.txtn = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.txtnumero = new System.Windows.Forms.TextBox();
+            this.lblprimo = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnrespuesta = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblCambio = new System.Windows.Forms.Label();
+            this.txtpago = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtpagar = new System.Windows.Forms.TextBox();
+            this.btnPagar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.LightGray;
+            this.groupBox1.Controls.Add(this.btnRestirar);
+            this.groupBox1.Controls.Add(this.txtCantidad);
+            this.groupBox1.Controls.Add(this.lblRespuesta);
+            this.groupBox1.Controls.Add(this.lblCantidad);
+            this.groupBox1.Location = new System.Drawing.Point(11, 16);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(345, 215);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Cajero Automatico ";
+            // 
+            // btnRestirar
+            // 
+            this.btnRestirar.Location = new System.Drawing.Point(19, 138);
+            this.btnRestirar.Name = "btnRestirar";
+            this.btnRestirar.Size = new System.Drawing.Size(75, 23);
+            this.btnRestirar.TabIndex = 7;
+            this.btnRestirar.Text = "Restirar";
+            this.btnRestirar.UseVisualStyleBackColor = true;
+            this.btnRestirar.Click += new System.EventHandler(this.btnRestirar_Click);
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(95, 33);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(100, 20);
+            this.txtCantidad.TabIndex = 6;
+            // 
+            // lblRespuesta
+            // 
+            this.lblRespuesta.AutoSize = true;
+            this.lblRespuesta.Location = new System.Drawing.Point(201, 37);
+            this.lblRespuesta.Name = "lblRespuesta";
+            this.lblRespuesta.Size = new System.Drawing.Size(58, 13);
+            this.lblRespuesta.TabIndex = 5;
+            this.lblRespuesta.Text = "Respuesta";
+            // 
+            // lblCantidad
+            // 
+            this.lblCantidad.AutoSize = true;
+            this.lblCantidad.Location = new System.Drawing.Point(-2, 37);
+            this.lblCantidad.Name = "lblCantidad";
+            this.lblCantidad.Size = new System.Drawing.Size(91, 13);
+            this.lblCantidad.TabIndex = 4;
+            this.lblCantidad.Text = "Cantidad a pagar ";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.BackColor = System.Drawing.Color.Gainsboro;
+            this.groupBox4.Controls.Add(this.btnmio);
+            this.groupBox4.Controls.Add(this.txtn);
+            this.groupBox4.Controls.Add(this.label4);
+            this.groupBox4.Controls.Add(this.listBox1);
+            this.groupBox4.Location = new System.Drawing.Point(18, 261);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(311, 206);
+            this.groupBox4.TabIndex = 13;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Secuencia Fibonacci.";
+            // 
+            // btnmio
+            // 
+            this.btnmio.Location = new System.Drawing.Point(13, 112);
+            this.btnmio.Name = "btnmio";
+            this.btnmio.Size = new System.Drawing.Size(75, 23);
+            this.btnmio.TabIndex = 3;
+            this.btnmio.Text = "Mostrar.";
+            this.btnmio.UseVisualStyleBackColor = true;
+            this.btnmio.Click += new System.EventHandler(this.btnmio_Click);
+            // 
+            // txtn
+            // 
+            this.txtn.Location = new System.Drawing.Point(32, 71);
+            this.txtn.Name = "txtn";
+            this.txtn.Size = new System.Drawing.Size(100, 20);
+            this.txtn.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 35);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(148, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Ingrese los valores a Generar:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(164, 32);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(138, 160);
+            this.listBox1.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.Color.Gainsboro;
+            this.groupBox3.Controls.Add(this.txtnumero);
+            this.groupBox3.Controls.Add(this.lblprimo);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.btnrespuesta);
+            this.groupBox3.Location = new System.Drawing.Point(649, 16);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(263, 185);
+            this.groupBox3.TabIndex = 12;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Numeros Primos";
+            // 
+            // txtnumero
+            // 
+            this.txtnumero.Location = new System.Drawing.Point(117, 26);
+            this.txtnumero.Name = "txtnumero";
+            this.txtnumero.Size = new System.Drawing.Size(100, 20);
+            this.txtnumero.TabIndex = 3;
+            // 
+            // lblprimo
+            // 
+            this.lblprimo.AutoSize = true;
+            this.lblprimo.Location = new System.Drawing.Point(47, 149);
+            this.lblprimo.Name = "lblprimo";
+            this.lblprimo.Size = new System.Drawing.Size(64, 13);
+            this.lblprimo.TabIndex = 2;
+            this.lblprimo.Text = "Respuesta: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ingrese Numero:";
+            // 
+            // btnrespuesta
+            // 
+            this.btnrespuesta.Location = new System.Drawing.Point(36, 82);
+            this.btnrespuesta.Name = "btnrespuesta";
+            this.btnrespuesta.Size = new System.Drawing.Size(75, 23);
+            this.btnrespuesta.TabIndex = 0;
+            this.btnrespuesta.Text = "Mostrar";
+            this.btnrespuesta.UseVisualStyleBackColor = true;
+            this.btnrespuesta.Click += new System.EventHandler(this.btnrespuesta_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Gainsboro;
+            this.groupBox2.Controls.Add(this.lblCambio);
+            this.groupBox2.Controls.Add(this.txtpago);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.txtpagar);
+            this.groupBox2.Controls.Add(this.btnPagar);
+            this.groupBox2.Location = new System.Drawing.Point(376, 16);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(250, 226);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Pagar";
+            // 
+            // lblCambio
+            // 
+            this.lblCambio.AutoSize = true;
+            this.lblCambio.Location = new System.Drawing.Point(36, 138);
+            this.lblCambio.Name = "lblCambio";
+            this.lblCambio.Size = new System.Drawing.Size(61, 13);
+            this.lblCambio.TabIndex = 11;
+            this.lblCambio.Text = "Respuesta:";
+            // 
+            // txtpago
+            // 
+            this.txtpago.Location = new System.Drawing.Point(103, 61);
+            this.txtpago.Name = "txtpago";
+            this.txtpago.Size = new System.Drawing.Size(100, 20);
+            this.txtpago.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Paga con:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Cantidad a pagar:";
+            // 
+            // txtpagar
+            // 
+            this.txtpagar.Location = new System.Drawing.Point(103, 32);
+            this.txtpagar.Name = "txtpagar";
+            this.txtpagar.Size = new System.Drawing.Size(100, 20);
+            this.txtpagar.TabIndex = 7;
+            // 
+            // btnPagar
+            // 
+            this.btnPagar.Location = new System.Drawing.Point(39, 101);
+            this.btnPagar.Name = "btnPagar";
+            this.btnPagar.Size = new System.Drawing.Size(75, 23);
+            this.btnPagar.TabIndex = 4;
+            this.btnPagar.Text = "Pagar";
+            this.btnPagar.UseVisualStyleBackColor = true;
+            this.btnPagar.Click += new System.EventHandler(this.btnPagar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(944, 482);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnRestirar;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.Label lblRespuesta;
+        private System.Windows.Forms.Label lblCantidad;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button btnmio;
+        private System.Windows.Forms.TextBox txtn;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox txtnumero;
+        private System.Windows.Forms.Label lblprimo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnrespuesta;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lblCambio;
+        private System.Windows.Forms.TextBox txtpago;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtpagar;
+        private System.Windows.Forms.Button btnPagar;
     }
 }
 
--- julio_cajero_primo_fibonacci/Form1.cs
+++ julio_cajero_primo_fibonacci/Form1.cs
@@ -16,5 +16,100 @@ namespace julio_cajero_primo_fibonacci
         {
             InitializeComponent();
         }
+
+        private void btnRestirar_Click(object sender, EventArgs e)
+        {
+            {
+                double[] denominaciones = { 100, 50, 20, 10, 5, 1, 0.5, 0.25, 0.1, 0.05, 0.01 };
+                double cantidad = double.Parse(txtCantidad.Text);
+                sbyte n = 0;
+                String respuesta = "Respuesta: ";
+                foreach (double denominacion in denominaciones)
+                {
+                    while (denominacion <= cantidad)
+                    {
+                        n++;
+                        cantidad -= denominacion;
+                    }
+                    if (n > 0)
+                    {
+                        respuesta += n + (denominacion > 1 ? "billetes " : "monedas ") + denominacion + "\n";
+                    }
+                    n = 0;
+                }
+                lblRespuesta.Text = respuesta;
+            }
+        }
+
+        private void btnPagar_Click(object sender, EventArgs e)
+        {
+            double[] denominaciones = { 100, 50, 20, 10, 5, 1, 0.50, 0.10, 0.05, 0.01 };
+            double cantidad = double.Parse(txtpagar.Text);
+            double pago = double.Parse(txtpago.Text);
+            sbyte n = 0;
+            double resta = pago - cantidad;
+            string resp = "cambio \n";
+            foreach (double denominacion in denominaciones)
+            {
+                while (denominacion <= Math.Round(resta, 2))
+                {
+                    n++;
+
+                    resta -= denominacion;
+                }
+                if (n > 0)
+                {
+                    resp += n + (denominacion > 1 ? "Billete" : "Monedas") + denominacion + "\n";
+                }
+                n = 0;
+            }
+            lblCambio.Text = resp;
+        }
+
+        private void btnrespuesta_Click(object sender, EventArgs e)
+        {
+            int x, a, b = 0;
+            x = Convert.ToInt32(txtnumero.Text);
+
+            for (a = 1; a <= x; a++)
+            {
+
+                if (x % a == 0)
+                    b++;
+            }
+
+            if (b > 1)
+
+            {
+                lblprimo.Text = "El numero es primo.";
+            }
+            else
+            {
+                lblprimo.Text = "El numero no es primo.";
+            }
+        }
+
+        private void btnmio_Click(object sender, EventArgs e)
+        {
+            int x, a, b = 0;
+            x = Convert.ToInt32(txtnumero.Text);
+
+            for (a = 1; a <= x; a++)
+            {
+
+                if (x % a == 0)
+                    b++;
+            }
+
+            if (b > 1)
+
+            {
+                lblprimo.Text = "El numero es primo.";
+            }
+            else
+            {
+                lblprimo.Text = "El numero no es primo.";
+            }
+        }
     }
 }
